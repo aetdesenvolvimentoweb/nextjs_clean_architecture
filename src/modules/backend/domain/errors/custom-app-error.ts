6bd4eea2afe19287f2a7ab2cfe516ea8aa171c82
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-17 16:02:31 -0300

    feat: create a custom error to application

---
+++ src/modules/backend/domain/errors/custom-app-error.ts
@@ -0,0 +1,11 @@
+export class CustomAppError extends Error {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    super(message);
+    this.message = message;
+    this.statusCode = statusCode;
+    this.name = "CustomAppError";
+  }
+}
