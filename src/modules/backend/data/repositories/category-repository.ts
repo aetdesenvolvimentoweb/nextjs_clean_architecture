198cd29d91bc48882fdaf66d3df14376f0366613
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-17 18:41:05 -0300

    refactor: add getById method to category repository

--- src/modules/backend/data/repositories/category-repository.ts
+++ src/modules/backend/data/repositories/category-repository.ts
@@ -2,5 +2,6 @@ import { AddCategory, Category } from "@/modules/backend/domain/entities";
 
 export interface CategoryRepository {
   add: (data: AddCategory) => Promise<Category>;
+  getById: (id: string) => Promise<Category | null>;
   getByName: (name: string) => Promise<Category | null>;
 }
