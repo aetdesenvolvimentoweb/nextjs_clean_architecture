e56afb25638a7f6997cc5652faad069c13032cd6
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-17 17:01:22 -0300

    test: ensure that AddCategoryService return a category with the ID property

---
+++ __mocks__/modules/backend/data/repositories/mock-category-repository.ts
@@ -0,0 +1,12 @@
+import { CategoryRepository } from "@/modules/backend/data/repositories";
+import { AddCategory, Category } from "@/modules/backend/domain/entities";
+
+export class MockCategoryRepository implements CategoryRepository {
+  add = async (data: AddCategory): Promise<Category> => {
+    return { id: "any_id", ...data };
+  };
+
+  getByName = async (name: string): Promise<Category | null> => {
+    return null;
+  };
+}
---
+++ __tests__/modules/backend/data/services/category/add-category-service.spec.ts
@@ -0,0 +1,22 @@
+import { AddCategoryService } from "@/modules/backend/data/services/category/add-category-service";
+import { MockCategoryRepository } from "@/../__mocks__/modules/backend/data/repositories";
+
+interface SutResponse {
+  sut: AddCategoryService;
+}
+
+const makeSut = (): SutResponse => {
+  const categoryRepository = new MockCategoryRepository();
+  const sut = new AddCategoryService(categoryRepository);
+  return { sut };
+};
+
+describe("AddCategoryService", () => {
+  test("should be return a category with the ID property", async () => {
+    const { sut } = makeSut();
+
+    const category = await sut.add({ name: "any_category" });
+
+    expect(category).toHaveProperty("id");
+  });
+});
