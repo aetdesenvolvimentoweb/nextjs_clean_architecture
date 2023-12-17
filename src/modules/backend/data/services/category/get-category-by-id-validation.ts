7788fdf5797b471cea1df168496b0ecd87017f45
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-17 18:46:11 -0300

    feat: create validation to get category by id

---
+++ src/modules/backend/data/services/category/get-category-by-id-validation.ts
@@ -0,0 +1,27 @@
+import {
+  DuplicatedUniqueKeyValidation,
+  MissingParamValidation,
+  RegisteredIdValidation,
+} from "@/modules/backend/domain/validations";
+import { CategoryRepository } from "../../repositories";
+import { CustomAppError } from "@/modules/backend/domain/errors";
+
+export class GetCategoryByIdValidation
+  implements MissingParamValidation, RegisteredIdValidation
+{
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  checkMissing = (data: string): void => {
+    if (!data) {
+      throw new CustomAppError("Preencha o campo ID.");
+    }
+  };
+
+  checkExist = async (id: string): Promise<void> => {
+    const existsCategory = await this.categoryRepository.getById(id);
+
+    if (!existsCategory) {
+      throw new CustomAppError("Nenhuma categoria encontrada com esse ID.");
+    }
+  };
+}
