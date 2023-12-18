8ccc3f98419474fc3064f6081db823d1df933439
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-18 10:48:49 -0300

    feat: add get all method in category repository

--- src/modules/backend/data/repositories/category-repository.ts
+++ src/modules/backend/data/repositories/category-repository.ts
@@ -3,6 +3,7 @@ import { AddCategory, Category } from "@/modules/backend/domain/entities";
 export interface CategoryRepository {
   add: (data: AddCategory) => Promise<Category>;
   delete: (id: string) => Promise<Category>;
+  getAll: () => Promise<Category[]>;
   getById: (id: string) => Promise<Category | null>;
   getByName: (name: string) => Promise<Category | null>;
 }
