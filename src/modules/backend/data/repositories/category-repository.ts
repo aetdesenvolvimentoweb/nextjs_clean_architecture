ece4f3391575202ec54a8d26bdcdc0ac293ccb2b
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-18 09:56:12 -0300

    feat: add delete method to category repository

--- src/modules/backend/data/repositories/category-repository.ts
+++ src/modules/backend/data/repositories/category-repository.ts
@@ -2,6 +2,7 @@ import { AddCategory, Category } from "@/modules/backend/domain/entities";
 
 export interface CategoryRepository {
   add: (data: AddCategory) => Promise<Category>;
+  delete: (id: string) => Promise<Category>;
   getById: (id: string) => Promise<Category | null>;
   getByName: (name: string) => Promise<Category | null>;
 }
