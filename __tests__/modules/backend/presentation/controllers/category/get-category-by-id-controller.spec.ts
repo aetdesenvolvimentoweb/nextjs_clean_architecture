c2f4ce5f7c5a22e6a70226e2de8d03e22db78a97
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-20 10:41:11 -0300

    test: ensure that GetCategoryByIdController return 200 if a category is found

---
+++ __tests__/modules/backend/presentation/controllers/category/get-category-by-id-controller.spec.ts
@@ -0,0 +1,44 @@
+import { CategoryRepository } from "@/modules/backend/data/repositories";
+import { GetCategoryByIdController } from "@/modules/backend/presentation/controllers/category";
+import {
+  MockCategoryRepository,
+  MockIdValidator,
+} from "@/../__mocks__/modules/backend/data/repositories";
+import { CategoryValidation } from "@/modules/backend/data/validations";
+import { GetCategoryByIdService } from "@/modules/backend/data/services/category";
+import { missingParamError } from "@/modules/backend/data/helpers";
+import { serverError } from "@/modules/backend/presentation/helpers";
+
+interface SutResponse {
+  categoryRepository: CategoryRepository;
+  sut: GetCategoryByIdController;
+}
+
+const makeSut = (): SutResponse => {
+  const categoryRepository = new MockCategoryRepository();
+  const idValidator = new MockIdValidator();
+  const categoryValidation = new CategoryValidation(
+    categoryRepository,
+    idValidator
+  );
+  const getCategoryByIdService = new GetCategoryByIdService(
+    categoryRepository,
+    categoryValidation
+  );
+  const sut = new GetCategoryByIdController(getCategoryByIdService);
+
+  return { categoryRepository, sut };
+};
+
+describe("GetCategoryByIdController", () => {
+  test("should be return 200 if category is found", async () => {
+    const { categoryRepository, sut } = makeSut();
+    jest
+      .spyOn(categoryRepository, "getById")
+      .mockResolvedValueOnce({ id: "any_id", name: "any_category" });
+
+    const httpResponse = await sut.handle({ params: { id: "any_id" } });
+
+    expect(httpResponse.statusCode).toBe(200);
+  });
+});
