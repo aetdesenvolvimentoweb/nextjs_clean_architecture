19b5d0857339f4c797c2b34d9fdd29f064009204
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-20 08:26:24 -0300

    feat: ensure that AddCategoryController return 201 if a category is created

---
+++ src/modules/backend/presentation/controllers/category/add-category-controller.ts
@@ -0,0 +1,25 @@
+import { AddCategoryService } from "@/modules/backend/data/services/category";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+import { AddCategory } from "@/modules/backend/domain/entities";
+import { CustomAppError } from "@/modules/backend/domain/errors";
+
+export class AddCategoryController implements Controller {
+  constructor(private readonly addCategoryService: AddCategoryService) {}
+
+  handle = async (request: HttpRequest): Promise<HttpResponse> => {
+    try {
+      const data: AddCategory = request.body;
+      await this.addCategoryService.add(data);
+
+      return { statusCode: 201 };
+    } catch (error: any) {
+      if (error instanceof CustomAppError) {
+        return { error: error.message, statusCode: error.statusCode };
+      }
+      return {
+        error: `Um erro inesperado aconteceu: ${error.message}`,
+        statusCode: 500,
+      };
+    }
+  };
+}
