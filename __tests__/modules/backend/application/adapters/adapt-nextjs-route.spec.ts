c883cbca96917a3fd3747ea5c2eb913fd04bd397
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 15:19:15 -0300

    test: ensure that AdapNextjsRoute return 400 if AddCategoryController is provided without body

--- __tests__/modules/backend/application/adapters/adapt-nextjs-route.spec.ts
+++ __tests__/modules/backend/application/adapters/adapt-nextjs-route.spec.ts
@@ -2,17 +2,20 @@ import { adaptNextjsRoute } from "@/modules/backend/application/adapters";
 import { makeAddCategoryController } from "@/modules/backend/application/factories/controllers/category";
 
 describe("AdaptNextjsRoute", () => {
-  test("should be return 400 if incorrect data is provided", async () => {
-    const sut = await adaptNextjsRoute(makeAddCategoryController(), {
-      body: {},
+  test("should be return 400 if AddCategoryController is provided without body", async () => {
+    const request = new Request(new URL("http://any_url.com"), {
+      method: "POST",
     });
+    const sut = await adaptNextjsRoute(makeAddCategoryController(), request);
 
     expect(sut.status).toBe(400);
   });
-  test("should be return 201 if category was created", async () => {
-    const sut = await adaptNextjsRoute(makeAddCategoryController(), {
-      body: { name: "any_category" },
+  test("should be return 201 if AddCategoryController and correct data is provided", async () => {
+    const request = new Request(new URL("http://any_url.com"), {
+      method: "POST",
+      body: JSON.stringify({ name: "any_category" }),
     });
+    const sut = await adaptNextjsRoute(makeAddCategoryController(), request);
 
     expect(sut.status).toBe(201);
   });
