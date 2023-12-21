5d63b3e8e9ad3e79ad04e35c78c87db10c913e77
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 17:33:23 -0300

    feat: ensure that delete method of military api route return 400 if no registered id is provided

--- src/app/api/category/[id]/route.ts
+++ src/app/api/category/[id]/route.ts
@@ -1,5 +1,6 @@
 import { adaptNextjsRoute } from "@/modules/backend/application/adapters";
 import {
+  makeDeleteCategoryController,
   makeGetCategoryByIdController,
   makeUpdateCategoryController,
 } from "@/modules/backend/application/factories/controllers/category";
@@ -22,6 +23,17 @@ export const PUT = async (
 ): Promise<NextResponse> => {
   return await adaptNextjsRoute(
     makeUpdateCategoryController(),
+    request,
+    params
+  );
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: { id: string } }
+): Promise<NextResponse> => {
+  return await adaptNextjsRoute(
+    makeDeleteCategoryController(),
     request,
     params
   );
