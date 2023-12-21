bc596263613cfe829694ece59cf5630154e71301
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 17:33:39 -0300

    test: ensure that delete method of military api route return 400 if no registered id is provided

--- __tests__/app/api/category/[id]/route.spec.ts
+++ __tests__/app/api/category/[id]/route.spec.ts
@@ -1,4 +1,4 @@
-import { GET, PUT } from "@/app/api/category/[id]/route";
+import { DELETE, GET, PUT } from "@/app/api/category/[id]/route";
 import { ObjectId } from "mongodb";
 
 describe("CategoryAPIRoute", () => {
@@ -17,6 +17,15 @@ describe("CategoryAPIRoute", () => {
     });
     const any_id = new ObjectId().toString();
     const sut = await PUT(request, { params: { id: any_id } });
+
+    expect(sut.status).toBe(400);
+  });
+  test("should be DELETE API route return 400 if no registered is provided", async () => {
+    const request = new Request(new URL("http://any-url.com"), {
+      method: "DELETE",
+    });
+    const any_id = new ObjectId().toString();
+    const sut = await DELETE(request, { params: { id: any_id } });
 
     expect(sut.status).toBe(400);
   });
