0914c88df014b50d002f29322342be35e9a8de7a
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 15:38:08 -0300

    feat: ensure that GET method of category API route return 200

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,7 +1,14 @@
 import { adaptNextjsRoute } from "@/modules/backend/application/adapters";
-import { makeAddCategoryController } from "@/modules/backend/application/factories/controllers/category";
+import {
+  makeAddCategoryController,
+  makeGetAllCategoriesController,
+} from "@/modules/backend/application/factories/controllers/category";
 import { NextResponse } from "next/server";
 
 export const POST = async (request: Request): Promise<NextResponse> => {
   return await adaptNextjsRoute(makeAddCategoryController(), request);
+};
+
+export const GET = async (request: Request): Promise<NextResponse> => {
+  return await adaptNextjsRoute(makeGetAllCategoriesController(), request);
 };
