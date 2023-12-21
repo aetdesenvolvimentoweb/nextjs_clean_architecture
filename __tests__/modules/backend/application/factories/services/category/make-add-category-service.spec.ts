dde1b022335dcb0a0488d0075263261c39dfe146
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 15:47:50 -0300

    refactor: change describe group

--- __tests__/modules/backend/application/factories/services/category/make-add-category-service.spec.ts
+++ __tests__/modules/backend/application/factories/services/category/make-add-category-service.spec.ts
@@ -1,6 +1,6 @@
 import { makeAddCategoryService } from "@/modules/backend/application/factories/services/category";
 
-describe("MakeAddMilitaryRankService", () => {
+describe("MakeAddCategoryService", () => {
   test("should be create an instance of AddCategoryService", () => {
     const sut = makeAddCategoryService();
 
