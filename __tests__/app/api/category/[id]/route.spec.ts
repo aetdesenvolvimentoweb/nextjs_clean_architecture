c9ddca26f3fb9d0654813a1c13d1af432ca20de7
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 17:12:18 -0300

    test: ensure that put method of military api route return 400 if no registered id is provided

--- __tests__/app/api/category/[id]/route.spec.ts
+++ __tests__/app/api/category/[id]/route.spec.ts
@@ -1,4 +1,4 @@
-import { GET } from "@/app/api/category/[id]/route";
+import { GET, PUT } from "@/app/api/category/[id]/route";
 import { ObjectId } from "mongodb";
 
 describe("CategoryAPIRoute", () => {
@@ -8,6 +8,15 @@ describe("CategoryAPIRoute", () => {
     });
     const any_id = new ObjectId().toString();
     const sut = await GET(request, { params: { id: any_id } });
+
+    expect(sut.status).toBe(400);
+  });
+  test("should be PUT API route return 400 if no registered is provided", async () => {
+    const request = new Request(new URL("http://any-url.com"), {
+      method: "PUT",
+    });
+    const any_id = new ObjectId().toString();
+    const sut = await PUT(request, { params: { id: any_id } });
 
     expect(sut.status).toBe(400);
   });
