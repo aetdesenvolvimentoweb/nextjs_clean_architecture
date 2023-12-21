74fedaf935e352f0f3f7df00715e944a58e65f44
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 09:33:40 -0300

    test: ensure that AdaptMongoIdValidator return true if valid id is provided

--- __tests__/modules/backend/application/adapters/adapt-mongo-id-validator.spec.ts
+++ __tests__/modules/backend/application/adapters/adapt-mongo-id-validator.spec.ts
@@ -1,9 +1,16 @@
 import { AdaptMongoIdValidator } from "@/modules/backend/application/adapters";
+import { ObjectId } from "mongodb";
 
 describe("AdaptMongoIdValidator", () => {
   test("should be return false if invalid id is provided", () => {
     const sut = new AdaptMongoIdValidator();
 
     expect(sut.isValid("invalid_id")).toBe(false);
+  });
+  test("should be return true if valid id is provided", () => {
+    const sut = new AdaptMongoIdValidator();
+    const valid_id = new ObjectId().toString();
+
+    expect(sut.isValid(valid_id)).toBe(true);
   });
 });
