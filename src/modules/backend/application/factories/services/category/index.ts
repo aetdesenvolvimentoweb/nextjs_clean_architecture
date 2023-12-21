523541669a3f10a1d1bd0f2339341794cc2fe13d
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 17:05:20 -0300

    feat: ensure that makeUpdateCategoryService return an instance of UpdateCategoryService

--- src/modules/backend/application/factories/services/category/index.ts
+++ src/modules/backend/application/factories/services/category/index.ts
@@ -1,3 +1,4 @@
 export * from "./make-add-category-service";
 export * from "./make-get-all-categories-service";
 export * from "./make-get-category-by-id-service";
+export * from "./make-update-category-service";
