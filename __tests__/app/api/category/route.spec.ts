6c4c1d0ccf9107b60b3965b229f64b6c9bb1fa3f
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-21 15:39:24 -0300

    test: ensure that GET method of category API route return 200

--- __tests__/app/api/category/route.spec.ts
+++ __tests__/app/api/category/route.spec.ts
@@ -1,4 +1,4 @@
-import { POST } from "@/app/api/category/route";
+import { GET, POST } from "@/app/api/category/route";
 
 describe("CategoryAPIRoute", () => {
   test("should be POST API route return 201 if correct data is provided", async () => {
@@ -9,5 +9,13 @@ describe("CategoryAPIRoute", () => {
     const sut = await POST(request);
 
     expect(sut.status).toBe(201);
+  });
+  test("should be GET API route return 200", async () => {
+    const request = new Request(new URL("http://any-url.com"), {
+      method: "GET",
+    });
+    const sut = await GET(request);
+
+    expect(sut.status).toBe(200);
   });
 });
